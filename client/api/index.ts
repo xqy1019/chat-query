d2b7ef58aee533da66551221bfcbb50af7588021
熊洋洋 <17362864761@163.com>
2023-06-26 13:54:20 +0800

    fix: fix site proxy slow

--- client/api/index.ts
+++ client/api/index.ts
@@ -1,10 +1,11 @@
+"use client"
 import axios from 'redaxios';
 
 const APi = axios.create({
 });
 
 export const backendApi = axios.create({
-  "baseURL":process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001"
+  "baseURL":  globalThis?.location?.protocol === 'https:' ? '/backend' : (process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001")
 });
 
 export default APi;
