7643b1c91d6a80877401e4956ff5b0de01f50001
熊洋洋 <17362864761@163.com>
2023-06-26 15:17:05 +0800

    fix: fix deploy

--- client/api/index.ts
+++ client/api/index.ts
@@ -1,11 +1,22 @@
-"use client"
+'use client';
+import { set } from 'lodash';
 import axios from 'redaxios';
 
-const APi = axios.create({
-});
+const APi = axios.create({});
 
 export const backendApi = axios.create({
-  "baseURL":  globalThis?.location?.protocol === 'https:' ? '/backend' : (process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001")
+    baseURL: '/backend',
 });
+
+(() => {
+    axios.get((process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001') + "/schema/all").then(
+        () => {
+        set(backendApi.defaults, "baseURL", process.env.NEXT_PUBLIC_BACKEND_URL)
+        },
+        err => {
+            console.log(err);
+        }
+    );
+})();
 
 export default APi;
